d86e97a307402c2145243f66e033ce85bfc171d1
Timothy Geary <tdgeary@gmail.com>
2016-06-05 05:53:09 -0400

    Modified pictures and GUI backend restructuring, multiple menus added

--- src/gui/SudokuGUI.java
+++ src/gui/SudokuGUI.java
@@ -1,42 +1,38 @@
 package gui;
 
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import model.SudokuModel;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import model.SudokuModel;
 
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
-//todo - experiment with making menus and selection screens
-//todo - bamboo color background with mild texture
+
 //todo - select puzzle before playing it, let them cycle through all 10000 and keep count
 //todo - original numbers are black and placed numbers are blue
 //todo - selecting a number option highlights all squares of that option (if all filled make the num option green)
 //todo - difficulty selection highlight animates it's movement between difficulties
-//todo - 'Select a number' band with pencil and eraser next to it
-//todo - bamboo chutes between inner squares
 //todo - make a home menu (have it resemble an inner square)
 //todo - highlighting a numbered square will make simliar numbered squares have yellow text and all boxes that matter to it get highlightedish
-//todo - fix the textout, don't change it for adding or removing numbers
 //todo - combine isGoal and isValid into Check and it says how many are left if isValid but not isGoal
 //todo - have the given numbers be grayed out and not be instantiated as the private class (make a gray.png)
 //todo - also have the option of solving the user's own puzzles they input themselves (backtrack for these)
 //todo - have an option to submit an answer and set off fireworks if it's correct
-//todo - have solve and a show solution button, solve starts from current puzzle, show solution starts from beginning
 //todo - allow scrawling small ints as personal reminders, 1 in top left, 9 bottom right, 4 middle left style
 //todo - play fireworks from ensemble upon successful solving, one firework if solved was used
+
 /**
  * The view class that implements a JavaFX UI. This class represents both
  * the view and controller portions of the UI. It is connected to the model
@@ -76,22 +72,22 @@ public class SudokuGUI extends Application implements Observer{
             this.col = col;
             this.num = num;
             switch(num){
-                case 1:
-                    Image pillarImg = new Image(getClass().getResourceAsStream("resources/pillarX.png"));
-                    ImageView pillarIcon = new ImageView(pillarImg);
-                    this.setGraphic(pillarIcon);
+                /*case 1:
+                    //Image pillarImg = new Image(getClass().getResourceAsStream("resources/pillarX.png"));
+                    //ImageView pillarIcon = new ImageView(pillarImg);
+                    //this.setGraphic(pillarIcon);
                     setButtonBackground(this, "white.png");
                     break;
                 case 2:
-                    Image beamImg = new Image(getClass().getResourceAsStream("resources/beam.png"));
-                    ImageView beamIcon = new ImageView(beamImg);
-                    this.setGraphic(beamIcon);
+                    //Image beamImg = new Image(getClass().getResourceAsStream("resources/beam.png"));
+                    //ImageView beamIcon = new ImageView(beamImg);
+                    //this.setGraphic(beamIcon);
                     setButtonBackground(this, "yellow.png");
                     break;
                 case 3:
-                    Image laserImg = new Image(getClass().getResourceAsStream("resources/laser.png"));
-                    ImageView laserIcon = new ImageView(laserImg);
-                    this.setGraphic(laserIcon);
+                    //Image laserImg = new Image(getClass().getResourceAsStream("resources/laser.png"));
+                    //ImageView laserIcon = new ImageView(laserImg);
+                    //this.setGraphic(laserIcon);
                     setButtonBackground(this, "yellow.png");
                     break;
                 case 4:
@@ -105,7 +101,7 @@ public class SudokuGUI extends Application implements Observer{
                 case 8:
                     break;
                 case 9:
-                    break;
+                    break;*/
                 default:
                     Image whiteImg = new Image(getClass().getResourceAsStream("resources/white.png"));
                     ImageView whiteIcon = new ImageView(whiteImg);
@@ -128,40 +124,139 @@ public class SudokuGUI extends Application implements Observer{
      */
     private void setButtonBackground(Button button, String bgImgName){
         BackgroundImage backgroundImage = new BackgroundImage(
-                new Image( getClass().getResource("resources/" + bgImgName).toExternalForm() ),
+                new Image( getClass().getResourceAsStream("resources/" + bgImgName) ),
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
-                BackgroundSize.DEFAULT);
+                new BackgroundSize(100, 100, true, true, true, true) );
         Background background = new Background(backgroundImage);
         button.setBackground(background);
     }
 
-    @Override
-    public void init() throws Exception{
+    /**
+     * Sets up the starting menu screen
+     * @param stage - The stage to set up
+     */
+    private void setMenuScreen(Stage stage){
+        VBox window = new VBox();
+        Scene scene = new Scene(window);
+        Image img = new Image( getClass().getResourceAsStream("resources/light.jpg") );
+        BackgroundImage BI = new BackgroundImage(img,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT );
+        window.setBackground( new Background(BI) );
+        window.setSpacing( 70 );
+
+        Label spacing = new Label();
+        window.getChildren().add( spacing );
+
+        Image titleImage = new Image( getClass().getResourceAsStream("resources/sudoku_title.png") );
+        ImageView title = new ImageView( titleImage );
+        title.setPreserveRatio( true );
+        title.setFitWidth( 500 );
+        window.getChildren().add(title);
+
+        TilePane options = new TilePane();
+        options.setPrefColumns(3);
+        options.setPrefRows(3);
+        Image image = new Image( getClass().getResourceAsStream("resources/frame.png") );
+        BackgroundImage optionsBackground = new BackgroundImage( image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                new BackgroundSize( 500, 500, false, false, true, true ) );
+        options.setBackground( new Background( optionsBackground ) );
+        window.getChildren().add(options);
+
+        //blank squares
+        Button option2 = new Button();
+        Button option4 = new Button();
+        Button option6 = new Button();
+        Button option8 = new Button();
+        setButtonBackground( option2, "gray.png" );
+        setButtonBackground( option4, "gray.png" );
+        setButtonBackground( option6, "gray.png" );
+        setButtonBackground( option8, "gray.png" );
+
+        Button help = new Button("Help");
+        help.setOnAction(e -> setHelpScreen(stage));
+        setButtonBackground( help, "white.png" );
+
+        Button start = new Button("Play");
+        start.setOnAction(e -> setDifficultySelectionScreen(stage));
+        setButtonBackground( start, "white.png" );
+
+        Button about = new Button("About");
+        about.setOnAction(e -> setAboutScreen(stage));
+        setButtonBackground( about, "white.png" );
+
+        Button donate = new Button("Donate");
+        setButtonBackground( donate, "white.png" );
+
+        Button quit = new Button("Quit");
+        quit.setOnAction(e -> System.exit(0));
+        setButtonBackground( quit, "white.png" );
+
+        options.getChildren().addAll( about, option2, help, option4, start, option6, donate, option8, quit );
+
+        stage.setScene(scene);
+    }
+
+    /**
+     * Sets up the help screen
+     * @param stage - The screen to set up
+     */
+    private void setHelpScreen(Stage stage){
+        //todo
+    }
+
+    /**
+     * Sets up the about screen
+     * @param stage - The screen to set up
+     */
+    private void setAboutScreen(Stage stage){
+        //todo
+    }
+
+    /**
+     * Selects a difficulty and sets filename based on that
+     * @param stage - The stage to set the selection screen on
+     */
+    private void setDifficultySelectionScreen(Stage stage){
         try{
             //todo - choose dificulty window here
-            //this.model = new SudokuModel(this.filename);
-            this.model = new SudokuModel("easy");
+            this.filename = "easy";
+            this.model = new SudokuModel(this.filename);
         }catch(FileNotFoundException fnfe){
             System.out.println(fnfe.getMessage());
             System.exit(-1);
         }
         this.model.addObserver(this);
+        setPuzzleSelectionScreen(stage);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception{
+    /**
+     * Selects a specific sudoku puzzle from the selected difficulty
+     * @param stage - The stage to set the puzzle selection screen on
+     */
+    private void setPuzzleSelectionScreen(Stage stage){
+        //todo
+        setGameScreen(stage);
+    }
+
+    /**
+     * Sets up the sudoku game once a file and linenumber are selected
+     * @param stage - The stage to set the game screen on
+     */
+    private void setGameScreen(Stage stage){
         this.errorPos = new int[2];
         this.errorPos[0] = 0;
         this.errorPos[1] = 0;
 
         BorderPane window = new BorderPane();
-        Scene cena = new Scene(window);
+        Scene scene = new Scene(window);
 
-        Path path = Paths.get(this.filename);
-        String file = path.getFileName().toString();
-        Text status = new Text( file + " loaded");
+        Text status = new Text( filename.substring(0, 1).toUpperCase() + filename.substring(1) + " selected");
         this.status = status;
         window.setTop(status);
         BorderPane.setAlignment(status, Pos.CENTER);
@@ -188,12 +283,12 @@ public class SudokuGUI extends Application implements Observer{
         Button solve = new Button("Solve");
         solve.setOnAction(e -> {
             try {
-                model.solve(true);// todo - Platform.runLater(Runnable r), runs operation as a thread in the background
+                model.solve(true);
             }catch(FileNotFoundException fnfe){ model.textout = "The file was deleted"; }});
         Button restart = new Button("Restart");
         restart.setOnAction(e -> {
             //todo - do this
-            this.model.textout = file + " has been reset";
+            this.model.textout = filename + " has been reset";
             this.model.announceChange();
         });
         Button newGame = new Button("New Game");
@@ -217,7 +312,7 @@ public class SudokuGUI extends Application implements Observer{
         window.setLeft(centering);
         BorderPane.setMargin(centering, new Insets(0, 11.25, 0, 0));
 
-        stage.setScene(cena);
+        stage.setScene(scene);
         stage.setMaxWidth(450);
         stage.setMinHeight(440);
         stage.setMaxHeight(500);
@@ -239,6 +334,16 @@ public class SudokuGUI extends Application implements Observer{
                 stage.setTitle("I'm sorry, it's Sudoku puzzle #" + model.lineNumber + ", good luck");
                 break;
         }
+        Image img = new Image( getClass().getResourceAsStream("resources/light.jpg") );
+        BackgroundImage BI = new BackgroundImage(img,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT );
+        window.setBackground( new Background(BI) );
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        setMenuScreen(stage);
         stage.show();
     }
 
