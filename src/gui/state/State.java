5fcf4378b10aa42847b6b6131c020d51cf7a4830
Timothy Geary <tdgeary@gmail.com>
2017-03-25 02:53:08 -0400

    Finished implementing the State interface

--- src/gui/state/State.java
+++ src/gui/state/State.java
@@ -1,9 +1,12 @@
 package gui.state;
 
+import gui.SudokuGUI;
+
 /**
  * The State interface that's used by SudokuGUI to implement the State design pattern
  * Each implementing class represents a different type of page that the user can be on in the GUI
  * @author Timothy Geary
  */
 public interface State{
+    void nextPage(SudokuGUI gui);
 }
