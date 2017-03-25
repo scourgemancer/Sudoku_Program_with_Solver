01e6da2f5aad471da8adcff23a61227e9910388a
Timothy Geary <tdgeary@gmail.com>
2017-03-25 02:48:23 -0400

    Added the State package and interface to begin implementing the State design pattern

---
+++ src/gui/state/State.java
@@ -0,0 +1,9 @@
+package gui.state;
+
+/**
+ * The State interface that's used by SudokuGUI to implement the State design pattern
+ * Each implementing class represents a different type of page that the user can be on in the GUI
+ * @author Timothy Geary
+ */
+public interface State{
+}
