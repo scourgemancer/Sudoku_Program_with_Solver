fdd2ce5b4bf29a519e5f44a6bb7009048261407d
Timothy Geary <tdgeary@gmail.com>
2017-04-04 02:15:13 -0400

    Added the instantaneous time category (<0.1s) as a goal for solving all of the puzzles

--- src/backtracking/AverageTimeFinder.java
+++ src/backtracking/AverageTimeFinder.java
@@ -13,6 +13,7 @@ import java.util.Optional;
  */
 public class AverageTimeFinder{
 
+    private static int totNumInstantaneous = 0;
     private static int totNumUnderHalfSec = 0;
     private static int totNumUnderSec = 0;
     private static int totNumUnderTwoSecs = 0;
@@ -24,6 +25,7 @@ public class AverageTimeFinder{
     private static int totNumOverMin = 0;
 
     public static void timeSolvingFor(String difficulty) throws FileNotFoundException{
+        int numInstantaneous = 0;
         int numUnderHalfSec = 0;
         int numUnderSec = 0;
         int numUnderTwoSecs = 0;
@@ -60,31 +62,33 @@ public class AverageTimeFinder{
             avgTime += timing;
             if(timing < bestTime) bestTime = timing;
             if(timing > worstTime) worstTime = timing;
-            if(timing < 0.5){     numUnderHalfSec++;      totNumUnderHalfSec++; }
-            else if(timing < 1){  numUnderSec++;          totNumUnderSec++;}
-            else if(timing < 2){  numUnderTwoSecs++;      totNumUnderTwoSecs++;}
-            else if(timing < 5){  numUnderFiveSecs++;     totNumUnderFiveSecs++;}
-            else if(timing < 10){ numUnderTenSecs++;      totNumUnderTenSecs++;}
-            else if(timing < 20){ numUnderTwentySecs++;   totNumUnderTwentySecs++;}
-            else if(timing < 30){ numUnderThirtySecs++;   totNumUnderThirtySecs++;}
-            else if(timing < 60){ numUnderMin++;          totNumUnderMin++;}
-            else{                 numOverMin++;           totNumOverMin++;}
+            if(timing < 0.1){      numInstantaneous++;     totNumInstantaneous++;}
+            else if(timing < 0.5){ numUnderHalfSec++;      totNumUnderHalfSec++; }
+            else if(timing < 1){   numUnderSec++;          totNumUnderSec++;}
+            else if(timing < 2){   numUnderTwoSecs++;      totNumUnderTwoSecs++;}
+            else if(timing < 5){   numUnderFiveSecs++;     totNumUnderFiveSecs++;}
+            else if(timing < 10){  numUnderTenSecs++;      totNumUnderTenSecs++;}
+            else if(timing < 20){  numUnderTwentySecs++;   totNumUnderTwentySecs++;}
+            else if(timing < 30){  numUnderThirtySecs++;   totNumUnderThirtySecs++;}
+            else if(timing < 60){  numUnderMin++;          totNumUnderMin++;}
+            else{                  numOverMin++;           totNumOverMin++;}
         }
 
         System.out.println("\n" + difficulty.substring(0, 1).toUpperCase() + difficulty.substring(1) + ":");
         System.out.println("\tBest:\t\t" + bestTime + " seconds");
         System.out.println("\tAverage:\t" + avgTime/10000 + " seconds");
         System.out.println("\tWorst:\t\t" + worstTime + " seconds\n");
-        System.out.println("\tUnder half of a second: " + numUnderHalfSec);
-        if(numUnderSec > 0){     System.out.println("\tUnder a second:         " + numUnderSec);
-        if(numUnderTwoSecs>0){   System.out.println("\tUnder two seconds:      " + numUnderTwoSecs);
-        if(numUnderFiveSecs>0){  System.out.println("\tUnder five seconds:     " + numUnderFiveSecs);
-        if(numUnderTenSecs>0){   System.out.println("\tUnder ten seconds:      " + numUnderTenSecs);
-        if(numUnderTwentySecs>0){System.out.println("\tUnder twenty seconds:   " + numUnderTwentySecs);
-        if(numUnderThirtySecs>0){System.out.println("\tUnder half of a minute: " + numUnderThirtySecs);
-        if(numUnderMin>0){       System.out.println("\tUnder a minute:         " + numUnderMin);
-        if(numOverMin>0){        System.out.println("\tOver a minute or more:  " + numOverMin);
-        }}}}}}}}
+        if(numInstantaneous>0){   System.out.println("\tInstantaneous (<0.1s):  " + numInstantaneous);
+        if(numUnderHalfSec>0){    System.out.println("\tUnder half of a second: " + numUnderHalfSec);
+        if(numUnderSec>0){        System.out.println("\tUnder a second:         " + numUnderSec);
+        if(numUnderTwoSecs>0){    System.out.println("\tUnder two seconds:      " + numUnderTwoSecs);
+        if(numUnderFiveSecs>0){   System.out.println("\tUnder five seconds:     " + numUnderFiveSecs);
+        if(numUnderTenSecs>0){    System.out.println("\tUnder ten seconds:      " + numUnderTenSecs);
+        if(numUnderTwentySecs>0){ System.out.println("\tUnder twenty seconds:   " + numUnderTwentySecs);
+        if(numUnderThirtySecs>0){ System.out.println("\tUnder half of a minute: " + numUnderThirtySecs);
+        if(numUnderMin>0){        System.out.println("\tUnder a minute:         " + numUnderMin);
+        if(numOverMin>0){         System.out.println("\tOver a minute or more:  " + numOverMin);
+        }}}}}}}}}}
     }
 
     public static void main(String[] args) throws FileNotFoundException{
@@ -103,16 +107,17 @@ public class AverageTimeFinder{
         int seconds = (int)(overallTime/1000)%60;
         int milli = (int)(overallTime%1000);
         System.out.println("\nOverall time for all puzzles: " + hours + ":" + minutes + ":" + seconds + ":" + milli);
-        System.out.println("Each puzzle took about " + ((overallTime / 1000) / 50000) + " seconds on average");
-        System.out.println("Under half of a second: " + totNumUnderHalfSec);
-        if(totNumUnderSec > 0){     System.out.println("\tUnder a second:         " + totNumUnderSec);
-        if(totNumUnderTwoSecs>0){   System.out.println("\tUnder two seconds:      " + totNumUnderTwoSecs);
-        if(totNumUnderFiveSecs>0){  System.out.println("\tUnder five seconds:     " + totNumUnderFiveSecs);
-        if(totNumUnderTenSecs>0){   System.out.println("\tUnder ten seconds:      " + totNumUnderTenSecs);
-        if(totNumUnderTwentySecs>0){System.out.println("\tUnder twenty seconds:   " + totNumUnderTwentySecs);
-        if(totNumUnderThirtySecs>0){System.out.println("\tUnder half of a minute: " + totNumUnderThirtySecs);
-        if(totNumUnderMin>0){       System.out.println("\tUnder a minute:         " + totNumUnderMin);
-        if(totNumOverMin>0){        System.out.println("\tOver a minute or more:  " + totNumOverMin + "\n");
-        }}}}}}}}
+        System.out.println("Average puzzle took about " + ((overallTime / 1000) / 50000) + " seconds");
+        if(totNumInstantaneous>0){   System.out.println("Instantaneous (<0.1s):  " + totNumInstantaneous);
+        if(totNumUnderHalfSec>0){    System.out.println("Under half of a second: " + totNumUnderHalfSec);
+        if(totNumUnderSec > 0){      System.out.println("Under a second:         " + totNumUnderSec);
+        if(totNumUnderTwoSecs>0){    System.out.println("Under two seconds:      " + totNumUnderTwoSecs);
+        if(totNumUnderFiveSecs>0){   System.out.println("Under five seconds:     " + totNumUnderFiveSecs);
+        if(totNumUnderTenSecs>0){    System.out.println("Under ten seconds:      " + totNumUnderTenSecs);
+        if(totNumUnderTwentySecs>0){ System.out.println("Under twenty seconds:   " + totNumUnderTwentySecs);
+        if(totNumUnderThirtySecs>0){ System.out.println("Under half of a minute: " + totNumUnderThirtySecs);
+        if(totNumUnderMin>0){        System.out.println("Under a minute:         " + totNumUnderMin);
+        if(totNumOverMin>0){         System.out.println("Over a minute or more:  " + totNumOverMin + "\n");
+        }}}}}}}}}}
     }
 }
