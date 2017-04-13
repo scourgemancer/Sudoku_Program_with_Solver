11311f40ca2b7cdf962ba012a59592c1f67181cc
Timothy Geary <tdgeary@gmail.com>
2017-04-12 22:29:33 -0400

    Added the state for the GUI's donation page

--- src/backtracking/AverageTimeFinder.java
+++ src/backtracking/AverageTimeFinder.java
@@ -13,6 +13,7 @@ import java.util.Optional;
  */
 public class AverageTimeFinder{
 
+    private static int totNumGoal = 0;
     private static int totNumInstantaneous = 0;
     private static int totNumUnderHalfSec = 0;
     private static int totNumUnderSec = 0;
@@ -25,6 +26,7 @@ public class AverageTimeFinder{
     private static int totNumOverMin = 0;
 
     public static void timeSolvingFor(String difficulty) throws FileNotFoundException{
+        int numGoal = 0;
         int numInstantaneous = 0;
         int numUnderHalfSec = 0;
         int numUnderSec = 0;
@@ -62,7 +64,8 @@ public class AverageTimeFinder{
             avgTime += timing;
             if(timing < bestTime) bestTime = timing;
             if(timing > worstTime) worstTime = timing;
-            if(timing < 0.1){      numInstantaneous++;     totNumInstantaneous++;}
+            if(timing < 0.01){     numGoal++;              totNumGoal++;}
+            else if(timing < 0.1){ numInstantaneous++;     totNumInstantaneous++;}
             else if(timing < 0.5){ numUnderHalfSec++;      totNumUnderHalfSec++; }
             else if(timing < 1){   numUnderSec++;          totNumUnderSec++;}
             else if(timing < 2){   numUnderTwoSecs++;      totNumUnderTwoSecs++;}
@@ -78,17 +81,17 @@ public class AverageTimeFinder{
         System.out.println("\tBest:\t\t" + bestTime + " seconds");
         System.out.println("\tAverage:\t" + avgTime/10000 + " seconds");
         System.out.println("\tWorst:\t\t" + worstTime + " seconds\n");
-        if(numInstantaneous>0){   System.out.println("\tInstantaneous (<0.1s):  " + numInstantaneous);
-        if(numUnderHalfSec>0){    System.out.println("\tUnder half of a second: " + numUnderHalfSec);
-        if(numUnderSec>0){        System.out.println("\tUnder a second:         " + numUnderSec);
-        if(numUnderTwoSecs>0){    System.out.println("\tUnder two seconds:      " + numUnderTwoSecs);
-        if(numUnderFiveSecs>0){   System.out.println("\tUnder five seconds:     " + numUnderFiveSecs);
-        if(numUnderTenSecs>0){    System.out.println("\tUnder ten seconds:      " + numUnderTenSecs);
-        if(numUnderTwentySecs>0){ System.out.println("\tUnder twenty seconds:   " + numUnderTwentySecs);
-        if(numUnderThirtySecs>0){ System.out.println("\tUnder half of a minute: " + numUnderThirtySecs);
-        if(numUnderMin>0){        System.out.println("\tUnder a minute:         " + numUnderMin);
-        if(numOverMin>0){         System.out.println("\tOver a minute or more:  " + numOverMin);
-        }}}}}}}}}}
+        if(numGoal>0)            System.out.println("\tGoal time (<0.01s):     " + numGoal);
+        if(numInstantaneous>0)   System.out.println("\tInstantaneous (<0.1s):  " + numInstantaneous);
+        if(numUnderHalfSec>0)    System.out.println("\tUnder half of a second: " + numUnderHalfSec);
+        if(numUnderSec>0)        System.out.println("\tUnder a second:         " + numUnderSec);
+        if(numUnderTwoSecs>0)    System.out.println("\tUnder two seconds:      " + numUnderTwoSecs);
+        if(numUnderFiveSecs>0)   System.out.println("\tUnder five seconds:     " + numUnderFiveSecs);
+        if(numUnderTenSecs>0)    System.out.println("\tUnder ten seconds:      " + numUnderTenSecs);
+        if(numUnderTwentySecs>0) System.out.println("\tUnder twenty seconds:   " + numUnderTwentySecs);
+        if(numUnderThirtySecs>0) System.out.println("\tUnder half of a minute: " + numUnderThirtySecs);
+        if(numUnderMin>0)        System.out.println("\tUnder a minute:         " + numUnderMin);
+        if(numOverMin>0)         System.out.println("\tOver a minute or more:  " + numOverMin);
     }
 
     public static void main(String[] args) throws FileNotFoundException{
@@ -108,16 +111,16 @@ public class AverageTimeFinder{
         int milli = (int)(overallTime%1000);
         System.out.println("\nOverall time for all puzzles: " + hours + ":" + minutes + ":" + seconds + ":" + milli);
         System.out.println("Average puzzle took about " + ((overallTime / 1000) / 50000) + " seconds");
-        if(totNumInstantaneous>0){   System.out.println("Instantaneous (<0.1s):  " + totNumInstantaneous);
-        if(totNumUnderHalfSec>0){    System.out.println("Under half of a second: " + totNumUnderHalfSec);
-        if(totNumUnderSec > 0){      System.out.println("Under a second:         " + totNumUnderSec);
-        if(totNumUnderTwoSecs>0){    System.out.println("Under two seconds:      " + totNumUnderTwoSecs);
-        if(totNumUnderFiveSecs>0){   System.out.println("Under five seconds:     " + totNumUnderFiveSecs);
-        if(totNumUnderTenSecs>0){    System.out.println("Under ten seconds:      " + totNumUnderTenSecs);
-        if(totNumUnderTwentySecs>0){ System.out.println("Under twenty seconds:   " + totNumUnderTwentySecs);
-        if(totNumUnderThirtySecs>0){ System.out.println("Under half of a minute: " + totNumUnderThirtySecs);
-        if(totNumUnderMin>0){        System.out.println("Under a minute:         " + totNumUnderMin);
-        if(totNumOverMin>0){         System.out.println("Over a minute or more:  " + totNumOverMin + "\n");
-        }}}}}}}}}}
+        System.out.println("Goal time (<0.01s):     " + totNumGoal);
+        if(totNumInstantaneous>0)   System.out.println("Instantaneous (<0.1s):  " + totNumInstantaneous);
+        if(totNumUnderHalfSec>0)    System.out.println("Under half of a second: " + totNumUnderHalfSec);
+        if(totNumUnderSec > 0)      System.out.println("Under a second:         " + totNumUnderSec);
+        if(totNumUnderTwoSecs>0)    System.out.println("Under two seconds:      " + totNumUnderTwoSecs);
+        if(totNumUnderFiveSecs>0)   System.out.println("Under five seconds:     " + totNumUnderFiveSecs);
+        if(totNumUnderTenSecs>0)    System.out.println("Under ten seconds:      " + totNumUnderTenSecs);
+        if(totNumUnderTwentySecs>0) System.out.println("Under twenty seconds:   " + totNumUnderTwentySecs);
+        if(totNumUnderThirtySecs>0) System.out.println("Under half of a minute: " + totNumUnderThirtySecs);
+        if(totNumUnderMin>0)        System.out.println("Under a minute:         " + totNumUnderMin);
+        if(totNumOverMin>0)         System.out.println("Over a minute or more:  " + totNumOverMin + "\n");
     }
 }
---
+++ src/gui/state/DonateState.java
@@ -0,0 +1,15 @@
+package gui.state;
+
+import gui.SudokuGUI;
+
+/**
+ * This class sets up the GUI's donation payment option page using the State pattern
+ * The donation page just explains who they're donating to and provides the options to donate
+ * @author Timothy Geary
+ */
+public class DonateState implements State{
+    @Override
+    public void nextPage(SudokuGUI gui){
+
+    }
+}
