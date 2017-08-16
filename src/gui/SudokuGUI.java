ac7e85ec9bb2c58d3af2b4b3f3f9c0dbf910b9ac
Timothy Geary <tdgeary@gmail.com>
2017-08-16 00:09:16 -0400

    Determined screen variable to be redundant

--- src/gui/SudokuGUI.java
+++ src/gui/SudokuGUI.java
@@ -77,9 +77,6 @@ public class SudokuGUI extends Application implements Observer{
 	/** The current state(page) that the gui is on **/
 	private State currentState;
 
-	/** Screen variable */
-	Screen screen;
-
 	/** A setter for the currentState variable **/
 	public void setState( State newState ){
 		currentState = newState;
@@ -98,43 +95,6 @@ public class SudokuGUI extends Application implements Observer{
             this.row = row;
             this.col = col;
             this.num = num;
-            switch(num){
-                /*case 1:
-                    //Image pillarImg = new Image(getClass().getResourceAsStream("resources/pillarX.png"));
-                    //ImageView pillarIcon = new ImageView(pillarImg);
-                    //this.setGraphic(pillarIcon);
-                    setButtonBackground(this, "white.png");
-                    break;
-                case 2:
-                    //Image beamImg = new Image(getClass().getResourceAsStream("resources/beam.png"));
-                    //ImageView beamIcon = new ImageView(beamImg);
-                    //this.setGraphic(beamIcon);
-                    setButtonBackground(this, "yellow.png");
-                    break;
-                case 3:
-                    //Image laserImg = new Image(getClass().getResourceAsStream("resources/laser.png"));
-                    //ImageView laserIcon = new ImageView(laserImg);
-                    //this.setGraphic(laserIcon);
-                    setButtonBackground(this, "yellow.png");
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;*/
-                default:
-                    //Image whiteImg = new Image(getClass().getResourceAsStream("resources/white.png"));
-                    //ImageView whiteIcon = new ImageView(whiteImg);
-                    //this.setGraphic(whiteIcon);
-                    //setButtonBackground(this, "white.png");
-            }
             this.setOnAction(e -> this.pressed());
         }
 
